14fec534721ccb48302226e4c9676be598c0d4be
3nupurs <3nupur@gmail.com>
2022-03-28 20:23:05 +0530

    Images Change

--- model/checkCart.js
+++
@@ -1,8 +0,0 @@
-const checkCart = (cartItems) => {
-    if (cartItems.length === 0) {
-      clearCart();
-      return true;
-    }
-    return false;
-  };
-  module.exports = checkCart;
--- test/checkCart.test.js
+++
@@ -1,6 +0,0 @@
-const checkCart = require('../model/checkCart');
-
-
-test('cart functionality test', () => {
-    expect(checkCart("5b6c6a7f01a7c38429530883")).toBe(false);
-});
